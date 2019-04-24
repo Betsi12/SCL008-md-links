c30cdaed3850213836df1d06b10478e4761c2c8f
Betsi12 <betsi.salas12@gmail.com>
2019-04-24 12:45:32 -0400

    Modificando test, index.js, definiendo md-links como comando de ejecucion en json

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-module.exports = () => {
-  // ...
-};
+#!/usr/bin/env node  // Indica al computador que se trabajara con node
+// Llamar a los módulos Node que se van a usar
+const path = require('path');
+const mdlink = require('./mdLinks.js');
+
+mdLinks.extracLinks(process.argv[2])
+.then((links)=>{
+  links.forEach(function(link){
+    console.log(`${link.file} ${link.href}`);
+    
+  });
+})
+
+
+
+
+
+  module.exports = mdLinks;
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,36 +1,29 @@
-const mdLinks = require('../index.js');
-
+const mdLinks = require('../md-links');
 
 describe('mdLinks', () => {
 
   it('Debería ser una función', () => {
-    expect(typeof mdLinks).toBe('function');
+    expect(typeof mdLinks).resolves.toEqual('function');
   });
 
-  it('Debería retornar un arreglo vacio si se pasa como argumento un archivo que no es .md',async  () => {
-    const check = await mdLinks(['../index.js'])
-    assert.deepEqual(check,[])
+  it('Debería retornar 8 links para el archivo ReadmeTest.md', () => {    
+    expect(mdLinks.extractLinks('./ReadmeTest.md')).resolves.toEqual(
+    [{href:'https://nodejs.org/docs/latest-v0.10.x/api/modules.html', text:'módulos (CommonJS)', file:'./ReadmeTest.md' },
+     {href:'https://nodejs.org/en/', text:'Node.js', file:'./ReadmeTest.md'},
+     {href:'https://nodejs.org/api/fs.html', text: 'file system', file: './ReadmeTest.md' },
+     {href:'https://nodejs.org/api/path.html', text: 'path', file: './ReadmeTest.md' },
+     {href:'https://nodejs.org/api/http.html#http_http_get_options_callback, parsin', text: 'http.get', file: './ReadmeTest.md'},
+     {href:'https://daringfireball.net/projects/markdown/syntax, CLI', text: 'markdown', file: './ReadmeTest.md'},
+     {href:'https://docs.npmjs.com/misc/scripts', text: 'npm-scripts', file: './ReadmeTest.md'},   
+     { href: 'https://semver.org/', text: 'semver', file: './ReadmeTest.md'}
+    ]);
   });
 
-  it('Debería retornar un arreglo vacio si se entrega como argumento una ruta inexistente',async  () => {
-    const check = await mdLinks(['./index.js'])
-    assert.deepEqual(check,[])
+  it('Debería retornar "Extension no válida" para el archivo index.js', async()  => {
+    await expect(mdLinks.extractLinks('../index.js')).rejects.toThrow("Extensión no es .md");
   });
-
-  it('Debería retornar una promesa, cuyo resolve sea un array de objetos, y cada objeto contenga la información del link, linea, archivo y validación', async () => {
-    const check = await mdLinks(['../README.md'],{validate : true})
-    const firstLink = {"mdString": '[Markdown]',"file": "C:\\Escritorio\\LABORATORIA\\ MarkdownLinks\\SCL008-md-links\\README.md", "line": , "link": "https://", "validation": true
-    }
-    assert.deepEqual(check[0],firstLink)
-  });
-
-  it('Debería recorrer todas las carpetas y archivos .md de la ruta que ingrese el usuario', async () => {
-      assert.deepEqual();
-  });
-
-  it('Debería recorrer todas las carpetas y archivos .md de la ruta que ingrese el usuario, y entregar la validación de los links si el usuario puso {validate:true} en las opciones', async () => {
-      assert.deepEqual();
-  });
-  
 });
+  
+
+ 
 
